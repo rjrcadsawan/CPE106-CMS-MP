46553c0013bcdb4ae86ed84fd20d5bd96b06714f
alaricespina <alaricsespina@gmail.com>
2022-03-03 19:45:12 +0800

    Gui commit
    Started working on GUI, moved folders to also include both projects

---
+++ MP GUI VERSION/MPGuiVersion/MPGuiVersion/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MPGuiVersion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string msg = "Login Button was clicked\n";
+            msg += $"Email Entered: {EmailBox.Text}\n";
+            msg += $"Password Entered: {PasswordBox.Text}";
+            MessageBox.Show(msg);
+            
+        }
+
+        private void Signup_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Signup Button was clicked");
+        }
+    }
+}

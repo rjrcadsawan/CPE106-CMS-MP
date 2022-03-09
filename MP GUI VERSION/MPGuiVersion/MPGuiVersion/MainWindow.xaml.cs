a84430dde33e8618c04d0bf4bbff80fb30ddb1a5
alaricespina <alaricsespina@gmail.com>
2022-03-09 21:54:23 +0800

    Update

--- MP GUI VERSION/MPGuiVersion/MPGuiVersion/MainWindow.xaml.cs
+++ MP GUI VERSION/MPGuiVersion/MPGuiVersion/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Data.SqlClient;
+
 namespace MPGuiVersion
 {
     /// <summary>
@@ -20,19 +22,104 @@ namespace MPGuiVersion
     /// </summary>
     public partial class MainWindow : Window
     {
+        string connection_string;
+        SqlConnection sql_conn;
+        SqlCommand sql_comm;
+        
+
         public MainWindow()
         {
+            
+
             InitializeComponent();
         }
 
+        private void InitializeConnections()
+        {
+
+            try
+            {
+                this.connection_string = "Data Source=ASUS-ACE;Initial Catalog=CMSData;Integrated Security=True";
+                this.sql_conn = new SqlConnection(connection_string);
+                this.sql_conn.Open();
+                MessageBox.Show("Connection with the CMSData was succesfully established");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An Exception has occurred: \n{ex}");
+            }
+            
+        }
         private void Login_Click(object sender, RoutedEventArgs e)
         {
+            InitializeConnections();
             string msg = "Login Button was clicked\n";
+            string email_text = EmailBox.Text;
+            string pass_text = PassBox.Password;
+            //string search_comm = "SELECT * FROM LogInDetails WHERE EmailAddress = '@email'";
+            string search_comm = $"SELECT * FROM LogInDetails WHERE EmailAddress = '{email_text}'";
+
             msg += $"Email Entered: {EmailBox.Text}\n";
+            msg += $"Password Entered: {PassBox.Password}";
             MessageBox.Show(msg);
-            Hide();
-            MainMenu MM = new MainMenu();
-            MM.Show();
+
+            //SQL Command and data retrieval
+
+
+            try
+            {
+                sql_comm = new SqlCommand(search_comm, this.sql_conn);
+                //sql_comm.Parameters.AddWithValue("@email", email_text);
+                SqlDataReader sql_read = sql_comm.ExecuteReader();
+                bool email_found = false;
+
+
+                while (sql_read.Read())
+                {
+
+                    email_found = true;
+                    if (sql_read["PasswordText"].ToString() == pass_text)
+                    {
+
+                        MessageBox.Show($"Entered Correct password, proceeding to main menu\n{sql_read["PasswordText"].ToString()} == {pass_text}");
+                        Hide();
+                        
+                        this.sql_conn.Close();
+                        MainMenu MM = new MainMenu();
+                        // Blocking Main Loop of MM
+                        
+                        MM.Show();
+                        Close();
+                        break;
+                        
+                        
+                        // Exits the program (since window was just hidden, this one closes it)
+                        
+
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Incorrect Password");
+                    }
+
+                }
+                
+                if (!email_found) {
+                    MessageBox.Show("Email Not Found");
+                }
+
+                
+
+
+            } catch (Exception ex)
+            {
+                MessageBox.Show($"Exception: {ex}");
+            }
+           
+            
+
+            
 
 
 
@@ -41,6 +128,8 @@ namespace MPGuiVersion
         private void Signup_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Signup Button was clicked");
+            SignupForm SF = new SignupForm();
+            SF.Show();
         }
     }
 }
--- MP GUI VERSION/MPGuiVersion/MPGuiVersion/SignupForm.xaml.cs
+++ MP GUI VERSION/MPGuiVersion/MPGuiVersion/SignupForm.xaml.cs
@@ -10,6 +10,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+using System.Data.SqlClient;
+
+
 namespace MPGuiVersion
 {
     /// <summary>
@@ -17,9 +20,74 @@ namespace MPGuiVersion
     /// </summary>
     public partial class SignupForm : Window
     {
+        string connection_string;
+        SqlConnection sql_conn;
+        SqlCommand sql_comm;
+
         public SignupForm()
         {
             InitializeComponent();
+            InitializeConnections();
+        }
+
+        private void InitializeConnections()
+        {
+            try
+            {
+                this.connection_string = "Data Source=ASUS-ACE;Initial Catalog=CMSData;Integrated Security=True";
+                this.sql_conn = new SqlConnection(connection_string);
+                this.sql_conn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An Exception has occurred: \n{ex}");
+            }
+
+        }
+
+        private void BackToLogin_Click(object sender, RoutedEventArgs e)
+        {
+            this.sql_conn.Close();
+            this.Close();
+        }
+
+        private void SignUp_Click(object sender, RoutedEventArgs e)
+        {
+            string first_name = FNBox.Text;
+            string middle_name = MNBox.Text;
+            string last_name = LNBox.Text;
+            string email_address = EmailBox.Text;
+            string password_text = PassBox.Text;
+            string search_comm = $"INSERT INTO [LogInDetails] VALUES ('{first_name}', '{middle_name}', '{last_name}', '{email_address}', '{password_text}')";
+
+
+            this.sql_comm = new SqlCommand(search_comm, this.sql_conn);
+            /*
+            sql_comm.Parameters.AddWithValue("@first_name", first_name);
+            sql_comm.Parameters.AddWithValue("@middle_nam", middle_name);
+            sql_comm.Parameters.AddWithValue("@last_name", last_name);
+            sql_comm.Parameters.AddWithValue("@email_address", email_address);
+            sql_comm.Parameters.AddWithValue("@password_text", password_text);
+            */
+            
+            SqlDataReader sql_read = sql_comm.ExecuteReader();
+            while (sql_read.Read())
+            {
+            }
+
+
+            MessageBox.Show($"You have succesfully registered {email_address}\n{search_comm}");
+            this.sql_conn.Close();
+            this.Close();
+        }
+
+        private void ResetFields_Click(object sender, RoutedEventArgs e)
+        {
+            FNBox.Text = "";
+            MNBox.Text = "";
+            LNBox.Text = "";
+            EmailBox.Text = "";
+            PassBox.Text = "";
         }
     }
 }

c15c668ade4b74c0f85807f858c8627092074e5b
Rence Cadsawan <78777373+rjrcadsawan@users.noreply.github.com>
2022-03-24 17:54:39 +0800

    Test

---
+++ MP GUI VERSION/MPGuiVersion/MPGuiVersion/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MPGuiVersion
+{
+    class Class1
+    {
+    }
+}
--- MP GUI VERSION/MPGuiVersion/MPGuiVersion/obj/Debug/netcoreapp3.1/EmailWindow.g.i.cs
+++ MP GUI VERSION/MPGuiVersion/MPGuiVersion/obj/Debug/netcoreapp3.1/EmailWindow.g.i.cs
@@ -151,13 +151,13 @@ namespace MPGuiVersion {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.14.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MPGuiVersion;component/emailwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MPGuiVersion;V1.0.0.0;component/emailwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\EmailWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -167,7 +167,7 @@ namespace MPGuiVersion {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.14.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
